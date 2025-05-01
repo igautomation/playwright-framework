b04fada99b5cc640a0fcae11f427f33ac6449104
Muhammad Ghouse Imran <muhammadimran711@gmail.com>
2025-05-01 12:22:20 +0530

    v 0.5

--- .history/.eslintrc_20250501120337.js
+++ .history/.eslintrc_20250501120337.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
+    indent: ['error', 2],
     quotes: ['error', 'single'],
     semi: ['error', 'always']
   }
--- .history/eslint.config_20250501120200.js
+++ .history/eslint.config_20250501120200.js
@@ -15,6 +15,7 @@ export default [
       }
     },
     rules: {
+      indent: ['error', 2],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
       'no-unused-vars': 'warn',
